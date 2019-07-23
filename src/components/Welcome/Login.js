61679451022f59656518a7c4dbaeec6893a211f9
joekennerly <joseph.kennerly@gmail.com>
2019-07-23 09:48:35 -0500

    error handling and check for user in fetch

--- src/components/Welcome/Login.js
+++ src/components/Welcome/Login.js
@@ -7,7 +7,21 @@ export default class Login extends Component {
   }
 
   handleChange = (event) => {
-    console.log(event)
+    if (event.key === "Enter") {
+      console.log(event)
+      //fetch
+      fetch(`http://localhost:5002/users?username=${event.target.value}`)
+        .then(res => res.json())
+        .then(user => {
+          if (user.length === 0) window.alert("no user found!")
+          else sessionStorage.setItem("activeUser", user[0].id)
+        console.log(user)
+      })
+      //check for matching
+      //post
+      //fetch
+      //set sessionStorage
+    }
   }
 
   render() {
@@ -17,7 +31,7 @@ export default class Login extends Component {
         <input
           autoFocus
           placeholder="username"
-          onChange={this.handleChange}
+          onKeyPress={this.handleChange}
         />
         <button onClick={this.handleChange}>
           login
