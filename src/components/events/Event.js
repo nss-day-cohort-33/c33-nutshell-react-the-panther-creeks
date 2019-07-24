ffe98295845db49813a2af7c0e7d37e3182519f5
Daniel Krusch <daniel.krusch@gmail.com>
2019-07-24 13:12:55 -0500

    Added date sorting for events

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react"
 import EventForm from "./EventForm"
-import { Link } from "react-router-dom"
-import APIManager from "../../modules/APIManager"
+import "./Event.css"
+import moment from "moment"
+import { isRegExp } from "util";
 // import "./Event.css"
 
 class Event extends Component {
@@ -9,38 +10,74 @@ class Event extends Component {
     events: []
   }
 
+  soon =
+  {
+      date: moment(),
+      changed: false
+  }
+
   render() {
       //if there is an active user
-      console.log(this.props)
       return (
         <React.Fragment>
           <EventForm {...this.props}/>
           <section className="events">
           {
-            this.props.events.map(event =>
-                <div key={event.id} className="card card--event">
-                        <div className="card-body">
-                            <div className="card-title">
-                                <h5>{event.name}</h5>
-                                <h6>{event.date}</h6>
-                                <h5>{event.location}</h5>
-                                <button
-                                    type="button"
-                                    className="btn btn-success"
-                                    onClick=
-                                    {() =>
-                                        {
-                                        this.props.history.push(`/events/${event.id}/edit`);
-                                        }
-                                    }>
-                                    Edit
-                                </button>
-                            <a href="#"
-                                // onClick={() => this.props.deleteItem("events", event.id)}
-                                className="card-link">Delete</a>
+            this.props.events.sort((a, b) => {
+                return moment(a.date).unix() - moment(b.date).unix()
+            }).map((event, i) =>
+            {
+                if ((moment().isSame(moment(event.date), "day") || moment(event.date).isAfter(moment())) && !this.soon.changed)
+                {
+                    this.soon.date = event.date
+                    this.soon.changed = true
+                }
+                if ((moment().isSame(moment(event.date), "day") || moment(event.date).isAfter(moment())) && moment(event.date).unix() <= moment(this.soon.date).unix())
+                {
+                    return <div key={event.id} className="card card--event" id="soonest">
+                            <div className="card-body">
+                                <div className="card-title">
+                                    <h5>{event.name}</h5>
+                                    <h6>{event.date}</h6>
+                                    <h5>{event.location}</h5>
+                                    <button
+                                        type="button"
+                                        className="btn btn-success"
+                                        onClick=
+                                        {() =>
+                                            {
+                                            this.props.history.push(`/events/${event.id}/edit`);
+                                            }
+                                        }>
+                                        Edit
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                }
+                else {
+                    return <div key={event.id} className="card card--event" id={`id-${i}`}>
+                            <div className="card-body">
+                                <div className="card-title">
+                                    <h5>{event.name}</h5>
+                                    <h6>{event.date}</h6>
+                                    <h5>{event.location}</h5>
+                                    <button
+                                        type="button"
+                                        className="btn btn-success"
+                                        onClick=
+                                        {() =>
+                                            {
+                                            this.props.history.push(`/events/${event.id}/edit`);
+                                            }
+                                        }>
+                                        Edit
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                }
+            }
             )
           }
           </section>
@@ -50,3 +87,5 @@ class Event extends Component {
 }
 
 export default Event
+
+
